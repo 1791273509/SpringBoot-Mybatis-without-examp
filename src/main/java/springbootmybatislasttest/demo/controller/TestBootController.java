b1d440c5e8a485ac8544c69dc8aabc8a4901a5b3
xwb <1791273509@qq.com>
2019-03-05 20:57:46 +0800

    整合mybaits不帶example文件的

---
+++ src/main/java/springbootmybatislasttest/demo/controller/TestBootController.java
@@ -0,0 +1,18 @@
+package springbootmybatislasttest.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springbootmybatislasttest.demo.entity.User;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/testboot")
+public class TestBootController {
+    @RequestMapping("getuser")
+    public User getUser() {
+        User user = new User();
+        user.setName("test");
+        return user;
+    }
+}
